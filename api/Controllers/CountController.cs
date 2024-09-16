36404e3ec6292911cba9f8cce9a0db80f04f812e
guslan1 <gustav.landin@hotmail.com>
2024-09-16 20:39:06 +0200

    Refactoring dictionary logic to separate methods

--- api/Controllers/CountController.cs
+++ api/Controllers/CountController.cs
@@ -6,12 +6,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-//    if (Request.Body == null)
-//     {
-//         return BadRequest("Request body cannot be null.");
-//     }
-
-
 namespace api.Controllers
 {
     public class CountController : ControllerBase
@@ -21,11 +15,9 @@ namespace api.Controllers
         [Consumes("text/plain")]
         public async Task<IActionResult> WordCount()
         {
-            // Läs in texten från Request Body
             using var reader = new StreamReader(Request.Body, Encoding.UTF8);
             var text = await reader.ReadToEndAsync();
 
-            // Kontrollera om texten är tom eller bara innehåller vita tecken
             if (string.IsNullOrWhiteSpace(text))
             {
                 return BadRequest("Input text cannot be empty or consist only of whitespace.");
@@ -33,10 +25,17 @@ namespace api.Controllers
 
             text = text.ToLower();
 
-            // Dela upp texten i ord med hjälp av Regex
             var words = Regex.Split(text, @"\W+");
 
-            // Vi använder dictionary för att enkelt och effektivt kunna söka och räkna antalet förekomster av varje ord
+            var wordCount = CountWords(words);
+
+            var topTenWords = GetTopTenWords(wordCount);
+
+            return Ok(topTenWords);
+        }
+
+        private Dictionary<string, int> CountWords(string[] words)
+        {
             var wordCount = new Dictionary<string, int>();
             foreach (var word in words)
             {
@@ -49,16 +48,16 @@ namespace api.Controllers
                     wordCount[word] = 1;
                 }
             }
+            return wordCount;
+        }
 
-            // Sortera orden efter deras förekomst och ta de tio största
-            var topTenWords = wordCount
+        private Dictionary<string, int> GetTopTenWords(Dictionary<string, int> wordCount)
+        {
+            return wordCount
                 .OrderByDescending(x => x.Value)
                 .ThenBy(x => x.Key)
                 .Take(10)
                 .ToDictionary(x => x.Key, x => x.Value);
-
-            // Returnera resultatet
-            return Ok(topTenWords);
         }
     }
 
